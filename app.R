f2d74e9d23e094c3a48308cd65c5b83612b6bda6
Kath Nicholls <nichollskc@gmail.com>
2021-05-14 17:33:06 +0100

    Further progress on visualisation

--- app.R
+++ app.R
@@ -9,6 +9,12 @@ source("biclust_utils.R")
 source("presnell.R")
 
 SSLB_result <- read_thresholded_factor_matrices(main_run)
+rownames(SSLB_result$B) <- gene_symbols
+rownames(SSLB_result$X) <- sample_names
+colnames(SSLB_result$X) <- paste("factor", 1:SSLB_result$K, sep="_")
+colnames(SSLB_result$B) <- paste("factor", 1:SSLB_result$K, sep="_")
+
+sample_info_with_fac <- cbind(sample_info, SSLB_result$X)
 
 ui <- fluidPage(
   titlePanel("Biclustering on Presnell sorted blood cell dataset"),
@@ -19,19 +25,36 @@ ui <- fluidPage(
     ),
     mainPanel(
       plotlyOutput("factorcontribution_heatmap"),
+      tableOutput("nz_samples_table"),
+      tableOutput("nz_genes_table"),
     )
   )
 )
 
 server <- function(input, output) {
   factor_contribution <- reactive({
-    calc_factor_contribution(SSLB_result, input$factor)
+    contribution <- calc_factor_contribution(SSLB_result, input$factor)
+    contribution
+  })
+  nz_samples <- reactive({
+    (SSLB_result$X[, input$factor] != 0)
+  })
+  nz_genes <- reactive({
+    (SSLB_result$B[, input$factor] != 0)
   })
   nz_factor_contribution <- reactive({
-    calc_nz_factor_contribution(SSLB_result, input$factor)
+    factor_contribution()#[nz_samples(), nz_genes()]
   })
   output$factorcontribution_heatmap <- renderPlotly({
     heatmaply(nz_factor_contribution())
+  })
+  
+  output$nz_samples_table <- renderTable({
+    sample_info[nz_samples()]
+  })
+  
+  output$nz_genes_table <- renderTable({
+    nz_genes()
   })
 }
 
--- presnell.R
+++ presnell.R
@@ -1,4 +1,12 @@
-main_run = "/Users/kath/docs/PhD/biclustering/E-GEOD-60424/results/SSLB/real/presnell/deseq_sf/raw/expressed/tensor/run_seed_8080_K_60"
+main_run <- "/Users/kath/docs/PhD/biclustering/E-GEOD-60424/results/SSLB/real/presnell/deseq_sf/raw/expressed/tensor/run_seed_8080_K_60"
 
-sample_info = read.csv("/Users/kath/docs/PhD/biclustering/E-GEOD-60424/data/real/presnell/deseq_sf/raw/expressed/tensor/sample_info_tidied.txt",
+sample_info <- read.csv("/Users/kath/docs/PhD/biclustering/E-GEOD-60424/data/real/presnell/deseq_sf/raw/expressed/tensor/sample_info_tidied.txt",
                        sep='\t')
+
+sample_names <- sample_info[['sample_description']]
+gene_info <- read.csv("/Users/kath/docs/PhD/biclustering/E-GEOD-60424/data/real/presnell/deseq_sf/raw/expressed/tensor/gene_info.txt",
+                      sep="\t",
+                      stringsAsFactors = FALSE)
+# Gene symbols, but for the 28 genes which do not have a unique gene symbol (14 which are duplicated)
+# add a suffix ___1 to the second such gene
+gene_symbols <- make.unique(gene_info$GeneSymbol, sep="___")
