aa7b360c3e6c804f875a76f7e3f92f39cad82a6a
Kath Nicholls <nichollskc@gmail.com>
2021-05-21 15:58:17 +0100

    Stop including max in the breaks

--- plot_utils.R
+++ plot_utils.R
@@ -3,37 +3,27 @@ neat_symmetric_cbar_breaks <- function(max_abs, n.breaks.approx=9) {
   
   # Identify the increment which is closest to being able to neatly fit the
   # required number of breaks
-  n.breaks.oneside <- floor(n.breaks.approx / 2) - 1
-  increment <- possible_increments[which.min(abs(floor(max_abs/possible_increments - 0.3) - n.breaks.oneside))]
-  print(max_abs/increment)
+  n.breaks.oneside <- floor(n.breaks.approx / 2)
+  increment <- possible_increments[which.min(abs(ceiling(max_abs/possible_increments) - n.breaks.oneside))]
   
   # Generate sequence using the increment. Start at last multiple of increment before max_abs,
   # but add adjustment of 0.3 to avoid this last multiple being too close to the max_abs value
-  last_break <- floor(max_abs/increment - 0.3) * increment
-  breaks <- c(-max_abs, seq(-last_break, last_break, increment), max_abs)
+  last_break <- ceiling(max_abs/increment) * increment
+  breaks <- seq(-last_break, last_break, increment)
   
-  #return(length(breaks))
   return(breaks)
 }
 
 neat_symmetric_cbar_breaks_log <- function(max_abs, n.breaks.approx=9) {
   all_breaks <- c(1, 10, 100, 1000, 10000, 100000)
   
-  n.breaks.oneside <- floor(n.breaks.approx / 2) - 1
+  n.breaks.oneside <- floor(n.breaks.approx / 2)
   last_break_index <- which(all_breaks > max_abs)[1]
-                               
-  last_break_index <- valid_break_indices[length(valid_break_indices)]
-  log_diff_to_max <- (log(all_breaks[last_break_index]) - log(max_abs))/log(max_abs)
-  if (abs(log_diff_to_max) < 1/n.breaks.oneside) {
-    last_break_index <- last_break_index - 1
-  }
-  
   first_break_index <- max(1, last_break_index - n.breaks.oneside + 1)
   
   positive_breaks <- all_breaks[seq(first_break_index, last_break_index)]
-  breaks <- c(-max_abs, rev(-positive_breaks), 0, positive_breaks, max_abs)
+  breaks <- c(rev(-positive_breaks), 0, positive_breaks)
   
-  #return(length(breaks))
   return(breaks)
 }
 
